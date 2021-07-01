befa0096ac69d08617677072e80a7c0e3da88e88
Miguel Garcia <72623443+migueldpgarcia@users.noreply.github.com>
2021-07-01 13:04:38 -0300

    Agora tem a opção de menu da turma em si

--- Menu_Turma.py
+++ Menu_Turma.py
@@ -4,6 +4,7 @@ from CLASS_Aluno import Aluno
 
 ALUNOS=[]
 ALUNOS_SEM_CLASS=[]
+ALUNOS_SEM_CLASS_ORDENADOS=[]
 PROFESSOR= Professor("Sem Professor")
 def f_adicionar_aluno ():
     while True:
@@ -51,23 +52,38 @@ def f_adicionar_professor ():
             PROFESSOR.name = nome_professor
 
 def f_alunos_ordem_alfabetica ():
-    ALUNOS_SEM_CLASS.sort ()
-    for i in range (0,len(ALUNOS_SEM_CLASS)):
-             print (str(ALUNOS_SEM_CLASS[i]))
+    ALUNOS_SEM_CLASS_ORDENADOS = ALUNOS_SEM_CLASS
+    ALUNOS_SEM_CLASS_ORDENADOS
+    print ("Os alunos são: " )
+    for i in range (0,len(ALUNOS_SEM_CLASS_ORDENADOS)):
+             print (str(ALUNOS_SEM_CLASS_ORDENADOS[i]))
+    print ("O Professor é: ")
+    print (str(PROFESSOR.name))
+
+def invalid_opt():
+  print("Invalid choice")
+
+options_turma = {"A":["Adicionar Aluno",f_adicionar_aluno], "B":["Remover Aluno",f_remover_aluno], "C":["Adiocionar Professor",f_adicionar_professor], "D": ["Listar alunos", f_alunos_ordem_alfabetica], "E": ["Dar nota aos alunos da turma", f_adicionar_alunonota]}
+
+while True:
+    print ("-=-=-=-=-=-=-=-")
+    for option in options_turma:
+         print(option+") "+options_turma.get(option)[0])
+    print ("-=-=-=-=-=-=-=-")
+
+    choise = input("Faça sua escolha: ")
+     
+    if choise == "":
+         break
+    else:
+         val = options_turma.get(choise)
+         if val is not None:
+             action = val[1]
+         else:
+            action = invalid_opt
+
+         action()
 
 
-    
 
-
-
-f_adicionar_aluno()
-f_remover_aluno()
-f_adicionar_alunonota()
-f_adicionar_professor()
-f_alunos_ordem_alfabetica ()
-
-
-
-
-print (str(PROFESSOR.name))
 
