27059c498298909d688f2d23a87031e0e77c4aa8
Miguel Garcia <72623443+migueldpgarcia@users.noreply.github.com>
2021-07-01 19:39:36 -0300

    Joguei as defs para dentro da CLASS_Turma, porem continua sem separar os alunos de cada turma

--- CLASS_Turma.py
+++ CLASS_Turma.py
@@ -9,34 +9,34 @@ class Turma:
     def __init__(self, name):
         self.name = name
     
-    def f_adicionar_aluno ():
+    def f_adicionar_aluno (self):
         while True:
             nome_aluno = str(input ("(Deixe Vazio para sair) Digite o nome do aluno a ser adicionado:"   ))
             if nome_aluno == "":
                 break
             else:
                 a = Aluno(nome_aluno)
-                ALUNOS.append (nome_aluno)
-                ALUNOS_SEM_CLASS.append (nome_aluno)
+                self.ALUNOS.append (nome_aluno)
+                self.ALUNOS_SEM_CLASS.append (nome_aluno)
              
 
-    def f_remover_aluno (t):
+    def f_remover_aluno (self):
         while True:
             nome_remover_aluno = str(input ("(Deixe Vazio para sair) Digite o nome do aluno a ser removido:"   ))
             if nome_remover_aluno == "":
                 break
             else:
-                QuantidadesAlunos = len (ALUNOS_SEM_CLASS)
+                QuantidadesAlunos = len (self.ALUNOS_SEM_CLASS)
                 for i in range (0,QuantidadesAlunos):
-                    if nome_remover_aluno == str((ALUNOS[i].name)):
-                        ALUNOS.pop(i)
+                    if nome_remover_aluno == str((self.ALUNOS[i].name)):
+                        self.ALUNOS.pop(i)
                         break
                 for g in range (0,QuantidadesAlunos):
-                    if nome_remover_aluno == str(ALUNOS_SEM_CLASS[g]):
-                        ALUNOS_SEM_CLASS.pop(g)
+                    if nome_remover_aluno == str(self.ALUNOS_SEM_CLASS[g]):
+                        self.ALUNOS_SEM_CLASS.pop(g)
                         break
 
-    def f_adicionar_alunonota (t):
+    def f_adicionar_alunonota (self):
         while True:
             nome_aluno_pela_nota = str(input ("(Deixe Vazio para sair) Digite o nome do aluno que queira dar a nota:"   ))
             if nome_aluno_pela_nota == "":
@@ -45,29 +45,29 @@ class Turma:
             if nota_do_aluno == "":
                 break
             else:
-                for i in range (0,len(ALUNOS_SEM_CLASS)):
-                    if nome_aluno_pela_nota == str((ALUNOS[i].name)):
-                        ALUNOS[i].nota = nota_do_aluno
+                for i in range (0,len(self.ALUNOS_SEM_CLASS)):
+                    if nome_aluno_pela_nota == str((self.ALUNOS[i].name)):
+                        self.ALUNOS[i].nota = nota_do_aluno
                         break
 
-    def f_adicionar_professor (t):
+    def f_adicionar_professor (self):
         while True:
             nome_professor = str(input ("(Deixe Vazio para sair) Digite o nome do professor:  "))
             if nome_professor == "":
                 break
             else:
-                PROFESSOR.name = nome_professor
+                self.name = nome_professor
 
-    def f_alunos_ordem_alfabetica (t):
+    def f_alunos_ordem_alfabetica (self):
         ALUNOS_SEM_CLASS_ORDENADOS = []
-        ALUNOS_SEM_CLASS_ORDENADOS = ALUNOS_SEM_CLASS
+        ALUNOS_SEM_CLASS_ORDENADOS = self.ALUNOS_SEM_CLASS
         ALUNOS_SEM_CLASS_ORDENADOS.sort()
         print ("Os alunos são: " )
         for i in range (0,len(ALUNOS_SEM_CLASS_ORDENADOS)):
                 print (str(ALUNOS_SEM_CLASS_ORDENADOS[i]))
         print ("O Professor é: ")
-        print (str(PROFESSOR.name))
+        print (str(self.PROFESSOR.name))
 
-    def f_mostrar_alunonota (t):
-        for i in range (0,len(ALUNOS_SEM_CLASS)):
-                print (str(ALUNOS[i].name) + " tem nota "+ str (ALUNOS[i].nota) )
+    def f_mostrar_alunonota (self):
+        for i in range (0,len(self.ALUNOS_SEM_CLASS)):
+                print (str(self.ALUNOS[i].name) + " tem nota "+ str (self.ALUNOS[i].nota) )
