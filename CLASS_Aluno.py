1150f677f4202106f9f84cff0d995a5a94f7704e
Miguel Garcia <72623443+migueldpgarcia@users.noreply.github.com>
2021-07-01 02:59:34 -0300

    Consegui botar a nota do aluno, acho que o len(ALUNOS) não estava bem definido ou o a3v == str (ALUNOS[i]), de qualquer forma, fiz uma nova lista sem classe e funcionou

--- CLASS_Aluno.py
+++ CLASS_Aluno.py
@@ -1,6 +1,7 @@
 class Aluno:
+  nota = 0
   def __init__(self, name):
     self.name = name
-    self.nota = 0
+    
 
 
--- Menu_Turma.py
+++ Menu_Turma.py
@@ -3,7 +3,7 @@ from CLASS_Professor import Professor
 from CLASS_Aluno import Aluno
 
 ALUNOS=[]
-ALUNOSNOTA=[]
+ALUNOS_SEM_CLASS=[]
 def f_adicionar_aluno ():
     while True:
          a1v = str(input ("(Deixe Vazio para sair) Digite o nome do aluno a ser adicionado:"   ))
@@ -11,6 +11,7 @@ def f_adicionar_aluno ():
              break
          else:
              ALUNOS.append (a1v)
+             ALUNOS_SEM_CLASS.append (a1v)
              ALUNOS[-1] = Aluno(a1v)
 
 def f_remover_aluno ():
@@ -19,31 +20,33 @@ def f_remover_aluno ():
         if a2v == "":
             break
         else:
-            for i in range (0,len(ALUNOS)):
-                if a2v == str((ALUNOS[i])):
+            for i in range (0,len(ALUNOS_SEM_CLASS)):
+                if a2v == str((ALUNOS_SEM_CLASS[i])):
                     ALUNOS.pop(i)
+                    ALUNOS_SEM_CLASS.pop(i)
                     break
 
 def f_adicionar_alunonota ():
     while True:
         a3v = str(input ("(Deixe Vazio para sair) Digite o nome do aluno que queira dar a nota:"   ))
+        if a3v == "":
+            break
         a1vN = str(input ("(Deixe Vazio para sair) Digite a nota deste aluno:"   ))
         if a1vN == "":
             break
         else:
-            for i in range (0, len(ALUNOS)):
-                if a3v == str(ALUNOS[i]):
+            for i in range (0,len(ALUNOS_SEM_CLASS)):
+                if a3v == str((ALUNOS_SEM_CLASS[i])):
                     ALUNOS[i].nota = a1vN
-                    print (ALUNOS[i].nota)
-                    #NaoEstouConseguindoGravarANotaDoAluno
-                    print (a1vN)
+                    print("a")
                     break
-                else: return
 
 f_adicionar_aluno()
 f_remover_aluno()
 f_adicionar_alunonota()
 
-print (str(ALUNOS[0].name))
-print (str(ALUNOS[0].nota))
+
+
+for i in range (0,len(ALUNOS_SEM_CLASS)):
+    print ("A nota do Aluno "+ str(ALUNOS[i].name)+ " é: " +str(ALUNOS[i].nota))
 
