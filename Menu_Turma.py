159580c92d4b48ff5f8522b448fec6d0c97674dd
Miguel Garcia <72623443+migueldpgarcia@users.noreply.github.com>
2021-07-01 12:14:32 -0300

    Mudei as variaveis e agora é possível  mostrar em ordem alfabética

--- Menu_Turma.py
+++ Menu_Turma.py
@@ -4,49 +4,70 @@ from CLASS_Aluno import Aluno
 
 ALUNOS=[]
 ALUNOS_SEM_CLASS=[]
+PROFESSOR= Professor("Sem Professor")
 def f_adicionar_aluno ():
     while True:
-         a1v = str(input ("(Deixe Vazio para sair) Digite o nome do aluno a ser adicionado:"   ))
-         if a1v == "":
+         nome_aluno = str(input ("(Deixe Vazio para sair) Digite o nome do aluno a ser adicionado:"   ))
+         if nome_aluno == "":
              break
          else:
-             ALUNOS.append (a1v)
-             ALUNOS_SEM_CLASS.append (a1v)
-             ALUNOS[-1] = Aluno(a1v)
+             ALUNOS.append (nome_aluno)
+             ALUNOS_SEM_CLASS.append (nome_aluno)
+             ALUNOS[-1] = Aluno(nome_aluno)
 
 def f_remover_aluno ():
     while True:
-        a2v = str(input ("(Deixe Vazio para sair) Digite o nome do aluno a ser removido:"   ))
-        if a2v == "":
+        nome_remover_aluno = str(input ("(Deixe Vazio para sair) Digite o nome do aluno a ser removido:"   ))
+        if nome_remover_aluno == "":
             break
         else:
             for i in range (0,len(ALUNOS_SEM_CLASS)):
-                if a2v == str((ALUNOS_SEM_CLASS[i])):
+                if nome_remover_aluno == str((ALUNOS_SEM_CLASS[i])):
                     ALUNOS.pop(i)
                     ALUNOS_SEM_CLASS.pop(i)
                     break
 
 def f_adicionar_alunonota ():
     while True:
-        a3v = str(input ("(Deixe Vazio para sair) Digite o nome do aluno que queira dar a nota:"   ))
-        if a3v == "":
+        nome_aluno_pela_nota = str(input ("(Deixe Vazio para sair) Digite o nome do aluno que queira dar a nota:"   ))
+        if nome_aluno_pela_nota == "":
             break
-        a1vN = str(input ("(Deixe Vazio para sair) Digite a nota deste aluno:"   ))
-        if a1vN == "":
+        nota_do_aluno = str(input ("(Deixe Vazio para sair) Digite a nota deste aluno:"   ))
+        if nota_do_aluno == "":
             break
         else:
             for i in range (0,len(ALUNOS_SEM_CLASS)):
-                if a3v == str((ALUNOS_SEM_CLASS[i])):
-                    ALUNOS[i].nota = a1vN
+                if nome_aluno_pela_nota == str((ALUNOS_SEM_CLASS[i])):
+                    ALUNOS[i].nota = nota_do_aluno
                     print("a")
                     break
+
+def f_adicionar_professor ():
+    while True:
+        nome_professor = str(input ("(Deixe Vazio para sair) Digite o nome do professor:  "))
+        if nome_professor == "":
+            break
+        else:
+            PROFESSOR.name = nome_professor
+
+def f_alunos_ordem_alfabetica ():
+    ALUNOS_SEM_CLASS.sort ()
+    for i in range (0,len(ALUNOS_SEM_CLASS)):
+             print (str(ALUNOS_SEM_CLASS[i]))
+
+
+    
+
+
 
 f_adicionar_aluno()
 f_remover_aluno()
 f_adicionar_alunonota()
+f_adicionar_professor()
+f_alunos_ordem_alfabetica ()
 
 
 
-for i in range (0,len(ALUNOS_SEM_CLASS)):
-    print ("A nota do Aluno "+ str(ALUNOS[i].name)+ " é: " +str(ALUNOS[i].nota))
+
+print (str(PROFESSOR.name))
 
