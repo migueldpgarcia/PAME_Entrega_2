815b828fd7889350285f8c06f4b0b2d5a271a87d
Miguel Garcia <72623443+migueldpgarcia@users.noreply.github.com>
2021-07-01 02:05:15 -0300

    Tentei realizar a função da nota, porém parece que não está gravando. Também transformei cade elemento da minha lista ALUNOS[i] na class Aluno

--- Menu_Turma.py
+++ Menu_Turma.py
@@ -10,8 +10,8 @@ def f_adicionar_aluno ():
          if a1v == "":
              break
          else:
-             a1 = Aluno(a1v)
-             ALUNOS.append (a1.name)
+             ALUNOS.append (a1v)
+             ALUNOS[-1] = Aluno(a1v)
 
 def f_remover_aluno ():
     while True:
@@ -24,15 +24,26 @@ def f_remover_aluno ():
                     ALUNOS.pop(i)
                     break
 
-def f_adicionar_alunonota ()
-        
-
-
+def f_adicionar_alunonota ():
+    while True:
+        a3v = str(input ("(Deixe Vazio para sair) Digite o nome do aluno que queira dar a nota:"   ))
+        a1vN = str(input ("(Deixe Vazio para sair) Digite a nota deste aluno:"   ))
+        if a1vN == "":
+            break
+        else:
+            for i in range (0, len(ALUNOS)):
+                if a3v == str(ALUNOS[i]):
+                    ALUNOS[i].nota = a1vN
+                    print (ALUNOS[i].nota)
+                    #NaoEstouConseguindoGravarANotaDoAluno
+                    print (a1vN)
+                    break
+                else: return
 
 f_adicionar_aluno()
 f_remover_aluno()
-print (ALUNOS)
+f_adicionar_alunonota()
 
-
-
+print (str(ALUNOS[0].name))
+print (str(ALUNOS[0].nota))
 
